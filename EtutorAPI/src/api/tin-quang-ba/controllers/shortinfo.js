9c508b076ec6ccfca94ca5e4d023c8fe6c0571e6
thientam075 <72021036+thientam075@users.noreply.github.com>
2022-01-11 14:22:01 +0700

    update shortinfo

--- EtutorAPI/src/api/tin-quang-ba/controllers/shortinfo.js
+++ EtutorAPI/src/api/tin-quang-ba/controllers/shortinfo.js
@@ -49,12 +49,9 @@ module.exports = {
       
       return ctx.badRequest("id is not exist");
     }
-    let query = `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, temp.star, temp. total_rating
-    FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, (SELECT DGTeacher.user_id as id, COUNT(DG) as total_rating, AVG(DG.star) as star
-                                              FROM danh_gias as DG, danh_gias_id_teacher_links as DGTeacher
-                                              WHERE DG.id = DGTeacher.danh_gia_id
-                                              GROUP BY DGTeacher.user_id) as temp
-    WHERE temp.id = U.id AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.id = ${queryObj}
+    let query = `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost
+    FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, 
+    WHERE AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.id = ${queryObj}
     ORDER BY U.fullname ASC`;
     var res = await strapi.db.connection.raw(query);
     return res;
