52b411ceb117b5d309114bb3378cb6fa01d97924
thientam075 <72021036+thientam075@users.noreply.github.com>
2022-01-11 08:21:28 +0700

    Delete column star, total_rating and fix query for this

--- EtutorAPI/src/api/tin-quang-ba/controllers/shortinfo.js
+++ EtutorAPI/src/api/tin-quang-ba/controllers/shortinfo.js
@@ -7,13 +7,13 @@
 module.exports = {
   async rankTutor(ctx) {
     var res = await strapi.db.connection.raw(
-      `SELECT
-        U.id,U.fullname,U.avatar, T.star, T.total_rating
-    FROM
-        "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT
-    WHERE
-        T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND T.star >= 4
-    ORDER BY T.star DESC
+      `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, temp.star, temp. total_rating
+      FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, (SELECT DGTeacher.user_id as id, COUNT(DG) as total_rating, AVG(DG.star) as star
+                                                FROM danh_gias as DG, danh_gias_id_teacher_links as DGTeacher
+                                                WHERE DG.id = DGTeacher.danh_gia_id
+                                                GROUP BY DGTeacher.user_id) as temp
+      WHERE temp.id = U.id AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND temp.star >= 4
+    ORDER BY temp.star DESC
     LIMIT 10`
     );
     return res;
@@ -22,21 +22,22 @@ module.exports = {
     const queryObj = ctx.request.query.name;
     let query = "";
     if (!queryObj || queryObj === "") {
-      query = `SELECT
-            U.id,U.fullname, U.avatar, T.star, T.total_rating
-        FROM
-            "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT
-        WHERE
-            T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id
-        ORDER BY U.fullname ASC`;
+      query = `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, temp.star, temp. total_rating
+      FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, (SELECT DGTeacher.user_id as id, COUNT(DG) as total_rating, AVG(DG.star) as star
+                                                FROM danh_gias as DG, danh_gias_id_teacher_links as DGTeacher
+                                                WHERE DG.id = DGTeacher.danh_gia_id
+                                                GROUP BY DGTeacher.user_id) as temp
+      WHERE temp.id = U.id AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id
+      ORDER BY U.fullname ASC`;
     } else {
-      query = `SELECT
-      U.id,U.fullname,U.avatar, T.star, T.total_rating
-  FROM
-      "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT
-  WHERE
-      T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.document_vectors @@ to_tsquery('${queryObj}')
-  ORDER BY T.star DESC`;
+      query = `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, temp.star, temp. total_rating
+      FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, (SELECT DGTeacher.user_id as id, COUNT(DG) as total_rating, AVG(DG.star) as star
+                                                FROM danh_gias as DG, danh_gias_id_teacher_links as DGTeacher
+                                                WHERE DG.id = DGTeacher.danh_gia_id
+                                                GROUP BY DGTeacher.user_id) as temp
+      WHERE temp.id = U.id AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.document_vectors @@ to_tsquery('${queryObj}')
+       
+  ORDER BY temp.star DESC`;
     }
     var res = await strapi.db.connection.raw(query);
     return res;
@@ -46,32 +47,23 @@ module.exports = {
     if (queryObj === null || queryObj === undefined) {
       return ctx.badRequest("id is not exist");
     }
-    let query = `SELECT
-            U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, T.star, T.total_rating
-        FROM
-            "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT
-        WHERE
-            T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.id = ${queryObj}
-        ORDER BY U.fullname ASC`;
+    let query = `SELECT U.id, U.fullname, U.email,U.avatar, T.profile, T.subjects, T.time, T.cost, temp.star, temp. total_rating
+    FROM "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT, (SELECT DGTeacher.user_id as id, COUNT(DG) as total_rating, AVG(DG.star) as star
+                                              FROM danh_gias as DG, danh_gias_id_teacher_links as DGTeacher
+                                              WHERE DG.id = DGTeacher.danh_gia_id
+                                              GROUP BY DGTeacher.user_id) as temp
+    WHERE temp.id = U.id AND T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id AND U.id = ${queryObj}
+    ORDER BY U.fullname ASC`;
     var res = await strapi.db.connection.raw(query);
     return res;
   },
   async findAllTutor(ctx) {
     let query = `SELECT
-    U.id, U.fullname, U.email, T.profile, T.subjects, T.time, T.cost, T.star, T.total_rating
+    U.id, U.fullname, U.email, T.profile, T.subjects, T.time, T.cost
 FROM
     "tin_quang_bas" as T, "up_users" as U, "tin_quang_bas_id_teacher_links" as UT
 WHERE
     T.id = UT."tin_quang_ba_id" AND UT."user_id" = U.id `;
-    var res = await strapi.db.connection.raw(query);
-    return res;
-  },
-  async updateIndex(ctx) {
-    let query = `
-    UPDATE 
-    up_users 
-SET 
-    document_vectors = (to_tsvector(fullname)) `;
     var res = await strapi.db.connection.raw(query);
     return res;
   },
